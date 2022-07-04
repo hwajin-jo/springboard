850927461d4986888bb11b3282f786fdd9c50e85
hwajin-jo <wa8784@naver.com>
2022-07-04 21:35:49 +0900

    4장 머스테치로 화면 구성

--- src/main/java/com/example/springboard/service/post/PostsService.java
+++ src/main/java/com/example/springboard/service/post/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springboard.service.post;
 
 import com.example.springboard.domain.post.Posts;
 import com.example.springboard.domain.post.PostsRepository;
+import com.example.springboard.web.dto.PostsListResponseDto;
 import com.example.springboard.web.dto.PostsResponseDto;
 import com.example.springboard.web.dto.PostsSaveRequestDto;
 import com.example.springboard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
