31f48536cd0e7c70b8cc3dcd41eec94079fd8523
hwajin-jo <wa8784@naver.com>
2022-07-06 23:34:50 +0900

    5장 최종완성

--- src/main/java/com/example/springboard/web/IndexController.java
+++ src/main/java/com/example/springboard/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.springboard.web;
 
+import com.example.springboard.config.auth.LoginUser;
+import com.example.springboard.config.auth.dto.SessionUser;
 import com.example.springboard.service.post.PostsService;
 import com.example.springboard.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
--- src/test/java/com/example/springboard/web/HelloControllerTest.java
+++ src/test/java/com/example/springboard/web/HelloControllerTest.java
@@ -5,38 +5,50 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
+import com.example.springboard.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-@WebMvcTest(HelloController.class)
-class HelloControllerTest {
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
+public class HelloControllerTest {
+
     @Autowired
     private MockMvc mvc;
+
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
-        //given
         String hello = "hello";
 
-        //when
         mvc.perform(get("/hello"))
-                //then
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
-    public void helloDto_리턴된다() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
-                get("/hello/dto")
-                        .param("name", name)
-                        .param("amount", String.valueOf(amount)))
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
-
     }
 }
